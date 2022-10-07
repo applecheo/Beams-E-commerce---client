cf5a312af71dc79dae9c736352d93fd3d97216ce
junjiecheo12 <wkcheojj@gmail.com>
2022-10-07 09:40:54 +0800

    first commit

---
+++ src/data/menCategories.ts
@@ -0,0 +1,144 @@
+export const MENS_CLOTHING = [
+  {
+    id: "7616",
+    name: "T-Shirts & Tanks",
+  },
+  {
+    id: "3602",
+    name: "Shirts",
+  },
+  {
+    id: "3606",
+    name: "Jackets & Coats",
+  },
+  {
+    id: "7617",
+    name: "Sweaters & Cardigans",
+  },
+  {
+    id: "5668",
+    name: "Hoodies & Sweatshirts",
+  },
+  {
+    id: "14273",
+    name: "Cargo Pants",
+  },
+  {
+    id: "16776",
+    name: "Tracksuits & Joggers",
+  },
+  {
+    id: "26090",
+    name: "Activewear",
+  },
+  {
+    id: "4208",
+    name: "Jeans",
+  },
+  {
+    id: "18797",
+    name: "Loungewear",
+  },
+  {
+    id: "4910",
+    name: "Pants & Chinos",
+  },
+  {
+    id: "4616",
+    name: "Polo shirts",
+  },
+  {
+    id: "7078",
+    name: "Shorts",
+  },
+  {
+    id: "16329",
+    name: "Socks",
+  },
+  {
+    id: "5678",
+    name: "Suits",
+  },
+  {
+    id: "14274",
+    name: "Sweatpants",
+  },
+  {
+    id: "13210",
+    name: "Swimwear",
+  },
+  {
+    id: "20317",
+    name: "Underwear",
+  },
+];
+
+export const MEN_SHOES = [
+  {
+    id: "Sneakers",
+    name: "5775",
+  },
+  {
+    id: "11247",
+    name: "Loafers",
+  },
+  {
+    id: "27116",
+    name: "Shoes",
+  },
+  {
+    id: "17514",
+    name: "Sliders & Flip flops",
+  },
+  {
+    id: "5774",
+    name: "Boots",
+  },
+  {
+    id: "5773",
+    name: "Dress shoes",
+  },
+  {
+    id: "14328",
+    name: "Slippers",
+  },
+  {
+    id: "16329",
+    name: "Socks",
+  },
+  {
+    id: "27179",
+    name: "Sports Sneakers",
+  },
+];
+
+export const MENS_ACCESSORIES = [
+  {
+    id: "9265",
+    name: "Bags",
+  },
+  {
+    id: "6517",
+    name: "Caps & Hats",
+  },
+  {
+    id: "5034",
+    name: "Jewelry",
+  },
+  {
+    id: "6519",
+    name: "Sunglasses",
+  },
+  {
+    id: "6520",
+    name: "Ties",
+  },
+  {
+    id: "6516",
+    name: "Wallets",
+  },
+  {
+    id: "19855",
+    name: "Watches",
+  },
+];
---
+++ src/data/womenCategories.ts
@@ -0,0 +1,131 @@
+export const WOMEN_CLOTHING = [
+  {
+    id: "8799",
+    name: "Dresses",
+  },
+
+  {
+    id: "4169",
+    name: "Tops",
+  },
+
+  {
+    id: "2641",
+    name: "Coats & Jackets",
+  },
+
+  {
+    id: "2637",
+    name: "Jumpers & Cardigans",
+  },
+
+  {
+    id: "11321",
+    name: "Hoodies & Sweatshirts",
+  },
+
+  {
+    id: "3630",
+    name: "Jeans",
+  },
+
+  {
+    id: "11318",
+    name: "Shirts & Blouses",
+  },
+
+  {
+    id: "9263",
+    name: "Shorts",
+  },
+
+  {
+    id: "2639",
+    name: "Skirts",
+  },
+
+  {
+    id: "7657",
+    name: "Socks & Tights",
+  },
+
+  {
+    id: "26091",
+    name: "Sportswear",
+  },
+];
+export const WOMEN_ACCESSORIES = [
+  {
+    id: "6448",
+    name: "Belts",
+  },
+
+  {
+    id: "25407",
+    name: "Caps",
+  },
+
+  {
+    id: "4545",
+    name: "Sunglasses",
+  },
+
+  {
+    id: "6452",
+    name: "Scarves",
+  },
+  {
+    id: "8730",
+    name: "Bags",
+  },
+  {
+    id: "4175",
+    name: "Jewelry",
+  },
+];
+export const WOMEN_SHOES = [
+  {
+    id: "6456",
+    name: "Trainers",
+  },
+
+  {
+    id: "6455",
+    name: "Boots",
+  },
+
+  {
+    id: "13692",
+    name: "Loafers",
+  },
+
+  {
+    id: "6459",
+    name: "Flats shoes",
+  },
+
+  {
+    id: "6461",
+    name: "Heels",
+  },
+
+  {
+    id: "6458",
+    name: "Sandals",
+  },
+
+  {
+    id: "15173",
+    name: "Sliders & Flip flops",
+  },
+
+  {
+    id: "6457",
+    name: "Slippers",
+  },
+
+  {
+    id: "27165",
+    name: "Sports Trainers",
+  },
+];
